9dd7eb9e4a9c09b02c5605924e85e08d08badfda
masutaro99 <maskenpa1001@gmail.com>
2021-02-08 18:31:16 +0900

    add: create post model and postscontroller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # get 'messages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :messages, only: :index, format: 'json' #=> この行を追加
+  resources :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_025044) do
+ActiveRecord::Schema.define(version: 2021_02_08_082837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2021_02_08_025044) do
   create_table "messages", force: :cascade do |t|
     t.string "title", null: false
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
