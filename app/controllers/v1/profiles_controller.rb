04d2ac2b9ebdac3c69ce894110bfcd704b394978
masutaro99 <maskenpa1001@gmail.com>
2021-02-11 15:48:34 +0900

    add: enable to show eacg profile and update profile when user login

--- app/controllers/v1/profiles_controller.rb
+++ app/controllers/v1/profiles_controller.rb
@@ -1,10 +1,26 @@
 # app/controllers/v1/profiles_controller.rb
 module V1
   class ProfilesController < ApiController
-    skip_before_action :authenticate_user_from_token!, only: [:index]
+    skip_before_action :authenticate_user_from_token!, only: [:index, :show]
+    before_action :set_target_profile, only: %i[show update]
+
     def index
       render json: Profile.all, each_serializer: V1::ProfileSerializer
     end
+    def show
+      render json: @profile, each_serializer: V1::ProfileSerializer
+    end
+    def update
+      @profile.update(profile_params)
+    end
 
+    private
+
+    def profile_params
+      params.require(:profile).permit(:nickName)
+    end
+    def set_target_profile
+      @profile = Profile.find(params[:id])
+    end
   end
 end
